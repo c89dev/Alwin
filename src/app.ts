f3ba8b6652dcef664183a0f26942714e26b3ac8f
c89dev <snarvei@live.com>
2026-02-20 07:31:03 +0100

    some layout

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+﻿function UpdateView(){
+    const app = document.getElementById('app');
+    if(app){
+        app.innerHTML = /*html*/`
+        <h1>Alwin - Stash it away</h1>
+        <input class="inputBar" type="text" placeholder="Search for item">
+        <button class="regBtn">Add item to inventory</button>
+        `;
+    }
+}
+
+UpdateView();
---
+++ src/controller/server.ts
@@ -0,0 +1,1 @@
+﻿import express from "express";
